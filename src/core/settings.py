8b3ba2f2c461bb53e6ca0e18131350e7cdfa0f62
rubio <rubiorubio1@yandex.ru>
2021-06-25 16:04:59 +0200

    start

--- src/core/settings.py
+++ src/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0jr4am5zglff=r1d811fxb!bys(#uz97@a61ziq77$@*eg=jtm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
