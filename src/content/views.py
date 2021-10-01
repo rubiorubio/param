068264026975d9b4ce26b300d5c996b4c492b693
Sergei Egorchenko <sergei.egorchenko@protos-tec.de>
2021-10-01 14:32:16 +0200

    Change titles and styling on homepage

--- src/content/views.py
+++ src/content/views.py
@@ -2,3 +2,79 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'content/index.html',{})
+
+####################################################
+
+from django.shortcuts import render
+from decouple import config
+import base64
+import requests
+from content import decode_jwt
+import os
+
+def home(request):
+    context = {}
+    try:
+        code = request.GET.get('code')
+        userData = getTokens(code)
+        context['name'] = userData['name']
+        context['status'] = 1
+
+        response = render(request, 'content/index.html', context)
+        response.set_cookie('sessiontoken', userData['id_token'], max_age=60*60*24, httponly=True)
+        return response
+    except:
+        token = getSession(request)
+        if token is not None:
+            userData = decode_jwt.lambda_handler(token,None)
+            context['name'] = userData['name']
+            context['status'] = 1
+            return render(request, 'content/index.html', context)
+        return render(request, 'content/index.html', {'status': 0})
+
+def getTokens(code):
+    TOKEN_ENDPOINT = config('TOKEN_ENDPOINT')
+    REDIRECT_URI = config('REDIRECT_URI')
+    CLIENT_ID = config('CLIENT_ID')
+    CLIENT_SECRET = config('CLIENT_SECRT')
+
+    encodeData = base64.b64encode(bytes(f"{CLIENT_ID}:{CLIENT_SECRET}","ISO-8859-1")).decode("ascii")
+
+    headers = {
+        'Content-Type': 'application/x-www-from-urlencoded',
+        'Authorization': f'Basic{encodeData}'
+    }
+
+    body = {
+        'grant_type': 'authorization_code',
+        'client_id': CLIENT_ID,
+        'code': code,
+        'redirect_uri': REDIRECT_URI,
+    }
+
+    response = requests.post(TOKEN_ENDPOINT, data=body,headers=headers )
+    
+    id_token = response.json()['id_token']
+
+    userData = decode_jwt.lambda_handler(id_token, None)
+
+    if not userData:
+        return False
+    
+    user = {
+        'id_token': id_token,
+        # 'name': userData['name'],
+        'emai': userData['email'],
+    }
+    return user
+    
+def getSession(request):
+    try:
+        response = request.COOKIES["sessiontoken"]
+        return response
+    except:
+        return None
+
+
+
+#########################################################################
